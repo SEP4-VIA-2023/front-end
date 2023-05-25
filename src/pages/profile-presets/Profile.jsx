0333867631f15c0938fdf5556708758776979ffa
Gabriel Vlad <gabriel.vlad@outlook.dk>
2023-05-25 21:19:00 +0200

    improvements to aspect and logic, ground work for the servo feature
    improvements to aspect and logic, making them different contains and putting in the ground work for adding the servo feature part

--- src/pages/profile-presets/Profile.jsx
+++ src/pages/profile-presets/Profile.jsx
@@ -82,120 +82,121 @@ const Profile = () => {
   };
 
 
- // Only render the component if profiles have been fetched
-if (profile === null) {
-  return <div>No profiles available.</div>;
-} else if (!profile) {
-  return <div>Loading...</div>;
-}
+  // Only render the component if profiles have been fetched
+  if (profile === null) {
+    return <div>No profiles available.</div>;
+  } else if (!profile) {
+    return <div>Loading...</div>;
+  }
 
 
   return (
-      <div className="profile">
-        <Sidebar className="sidebar"/> {/* This is the sidebar component */}
+    <div className="profile">
+      <Sidebar className="sidebar" /> {/* This is the sidebar component */}
 
-    <div className="profileContainer settings-container">
-      <h2 className="settings-title">Settings</h2>
-      
-      <div className="profile-selection">
-        <label htmlFor="profile-select">Select Profile:</label>
-        <select id="profile-select" onChange={handleSelect} value={profile.title}>
-          {profiles.map((profile, index) => (
-            <option key={index} value={profile.title}>{profile.title}</option>
-          ))}
-        </select>
+      <div className="settings-container">
+        <div className="profileContainer">
+          <h2 className="settings-title">Settings</h2>
+
+          <div className="profile-selection">
+            <label htmlFor="profile-select">Select Profile:</label>
+            <select id="profile-select" onChange={handleSelect} value={profile.title}>
+              {profiles.map((profile, index) => (
+                <option key={index} value={profile.title}>{profile.title}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="title-input">Name:</label>
+            <input
+              id="title-input"
+              type="text"
+              placeholder="Name of your profile"
+              name="title"
+              value={profile.title}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="co2-min-input">CO2 Min Value:</label>
+            <input
+              id="co2-min-input"
+              type="number"
+              placeholder="CO2 Min Value"
+              name="co2Min"
+              value={profile.co2Min || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="co2-max-input">CO2 Max Value:</label>
+            <input
+              id="co2-max-input"
+              type="number"
+              placeholder="CO2 Max Value"
+              name="co2Max"
+              value={profile.co2Max || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="humidity-min-input">Humidity Min Value:</label>
+            <input
+              id="humidity-min-input"
+              type="number"
+              placeholder="Humidity Min Value"
+              name="humidityMin"
+              value={profile.humidityMin || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="humidity-max-input">Humidity Max Value:</label>
+            <input
+              id="humidity-max-input"
+              type="number"
+              placeholder="Humidity Max Value"
+              name="humidityMax"
+              value={profile.humidityMax || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="temperature-min-input">Temperature Min Value:</label>
+            <input
+              id="temperature-min-input"
+              type="number"
+              placeholder="Temperature Min Value"
+              name="temperatureMin"
+              value={profile.temperatureMin || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="temperature-max-input">Temperature Max Value:</label>
+            <input
+              id="temperature-max-input"
+              type="number"
+              placeholder="Temperature Max Value"
+              name="temperatureMax"
+              value={profile.temperatureMax || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="button-container">
+            <button onClick={handleDiscard}>Discard</button>
+            <button onClick={handleSave}>Save</button>
+          </div>
+        </div>
       </div>
-
-      <div className="setting-item">
-        <label htmlFor="title-input">Name:</label>
-        <input
-          id="title-input"
-          type="text"
-          placeholder="Name of your profile"
-          name="title"
-          value={profile.title}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="setting-item">
-        <label htmlFor="co2-min-input">CO2 Min Value:</label>
-        <input
-          id="co2-min-input"
-          type="number"
-          placeholder="CO2 Min Value"
-          name="co2Min"
-          value={profile.co2Min || ""}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="setting-item">
-        <label htmlFor="co2-max-input">CO2 Max Value:</label>
-        <input
-          id="co2-max-input"
-          type="number"
-          placeholder="CO2 Max Value"
-          name="co2Max"
-          value={profile.co2Max || ""}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="setting-item">
-        <label htmlFor="humidity-min-input">Humidity Min Value:</label>
-        <input
-          id="humidity-min-input"
-          type="number"
-          placeholder="Humidity Min Value"
-          name="humidityMin"
-          value={profile.humidityMin || ""}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="setting-item">
-        <label htmlFor="humidity-max-input">Humidity Max Value:</label>
-        <input
-          id="humidity-max-input"
-          type="number"
-          placeholder="Humidity Max Value"
-          name="humidityMax"
-          value={profile.humidityMax || ""}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="setting-item">
-        <label htmlFor="temperature-min-input">Temperature Min Value:</label>
-        <input
-          id="temperature-min-input"
-          type="number"
-          placeholder="Temperature Min Value"
-          name="temperatureMin"
-          value={profile.temperatureMin || ""}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="setting-item">
-        <label htmlFor="temperature-max-input">Temperature Max Value:</label>
-        <input
-          id="temperature-max-input"
-          type="number"
-          placeholder="Temperature Max Value"
-          name="temperatureMax"
-          value={profile.temperatureMax || ""}
-          onChange={handleChange}
-        />
-      </div>
-
-
-      <div className="button-container">
-        <button onClick={handleDiscard}>Discard</button>
-        <button onClick={handleSave}>Save</button>
-      </div>
-    </div>
     </div>
   );
 };
