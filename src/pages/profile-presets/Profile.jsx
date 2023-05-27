da91a315065174d486b5bf424e5fbcdc37024793
Gabriel Vlad <73888724+gabrielvlad@users.noreply.github.com>
2023-05-27 14:49:25 +0200

    rewrote to use Axios, did behaviour tests manully and fixed where needed+fixed jest conflict with Axios library+added babel (#17)
    * Made profile-presets component
    
    The component needs to be able to:
    select profile scroll down
    set value for co2, humidity, temperature
    change unit of measurement from imperial to metrics and vice versa
    select how many graphs to display from drop down
    
    pressable buttons
    discard and save for resting the data to original state or save the preset
    
    ill continue to commit on it is completely done and functional.
    
    * Added link to component
    
    Included component into Sidebar
    included component into app.js
    
    * changes to profile
    
    made the profiles selectable and you can switch between them
    
    * Create Profile.test.js
    
    wrote UI tests for the component
    
    * middleware backend+shared resources+rewrote component to accomodate APIs
    
    added backend middleware for being able to write data not just read to endpoint APIs
    rewrote profile component to accommodate fetching and getting
    installed express for backend and cors for Cross-Origin Resource Sharing (so port 3000 can talk with port 3001 and we can request files, is due to the middle security that cors had to be installed, since browser considers 3000 and 3001 different origins)
    checked compatibility between dependencies and ensured it works as manual behavior testing
    
    * rewrote test for the profile component
    
    Installed jest-fetch-mock for being able to use async fetch testing when a backend sends data after the ui initial request otherwise testing directly for ui elements will fail
    
    * fixed routes+rewrote component to handle min and max values for each sensor data
    
    fixed routes,
    rewrote component to handle min and max values,
    rewrote conditional render function that tells if is data to change based on no data found or loading new data
    wrote new tests for the new added code
    
    * made sidebar be side by side
    
    added sidebar to be side by side with the profile-presets
    
    * improvements to aspect and logic, ground work for the servo feature
    
    improvements to aspect and logic, making them different contains and putting in the ground work for adding the servo feature part
    
    * servo feature+data consistency
    
    added servo feature
    added checkers for data consistency
    
    * Update Profile.jsx
    
    included checker for when no profile is left
    
    * rewrote to use axios, did behaviour tests manully
    
    added axios library since promise-based HTTP requests became more complex and more important to be able to communicate from our server middleware to the backend hosted server by the cloud team
    rewrote component to use the axios library, also did behavior tests manually for the new changes and applied any fix where was necessary
    
    * included babel due to jest error
    
    included babel to fix jest error
    
    * fixed jest conflict with axios library
    
    I told react to transform the import so it can read understand it
    afterwards I took out from the jest config the option for type since CRA does not support it
    therefore everything works now, and tests can be done without a problem.
    
    * devops workflow change to include server start
    
    server added and started before the tests are run to ensure there will be a 3001 listening
    
    ---------
    
    Co-authored-by: Bhupas Gautam <64760375+bhupas@users.noreply.github.com>
    Co-authored-by: Bhupas Gautam <bhupas@outlook.com>

--- src/pages/profile-presets/Profile.jsx
+++ src/pages/profile-presets/Profile.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./profile.scss";
+import axios from 'axios';
+
+
 const Profile = () => {
   const [profiles, setProfiles] = useState([]);
   const [profile, setProfile] = useState(null);
-  const [co2Data, setCo2Data] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/data')
-      .then(response => response.json())
-      .then(data => {
-        setProfiles(data);
-        setProfile(data[0]); // Use the first profile as the default
+    axios.get('http://localhost:3001/data')
+      .then(response => {
+        setProfiles(response.data);
+        setProfile(response.data[0]);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error('Error:', error);
       });
   }, []);
-
 
   const handleChange = (event) => {
     setProfile({
@@ -25,171 +27,191 @@ const Profile = () => {
   };
 
   const handleDiscard = () => {
-    // Remove profile from the local state
-    const remainingProfiles = profiles.filter(p => p.title !== profile.title);
-    setProfiles(remainingProfiles);
-  
-    // Check if there are remaining profiles
-    if (remainingProfiles.length > 0) {
-      // Select the first one from the remaining profiles
-      setProfile(remainingProfiles[0]);
+    if (profiles.length <= 1) {
+      alert("Stop, you are going to remain with no profiles at all if you delete the only one you have");
     } else {
-      // Set the profile state to null if no profiles are left
-      setProfile(null);
-    }
-  
-    // Delete the profile from the server
-    fetch(`http://localhost:3001/data/${profile.title}`, {
-      method: 'DELETE',
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  };
-  
+      axios.delete(`http://localhost:3001/data/${profile.title}`)
+        .then(response => {
+          console.log(response.data);
+          const remainingProfiles = profiles.filter(p => p.title !== profile.title);
+          setProfiles(remainingProfiles);
 
+          if (remainingProfiles.length > 0) {
+            setProfile(remainingProfiles[0]);
+          } else {
+            setProfile(null);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  };
 
   const handleSave = () => {
-    console.log("Saved: ", profile);
-    setCo2Data(oldData => [...oldData, { name: profile.title, CO2: parseInt(profile.co2) }]);
+    const existingProfileIndex = profiles.findIndex((p) => p.title === profile.title);
 
-    fetch('http://localhost:3001/data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(profile)
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setProfiles(oldProfiles => [...oldProfiles, profile]);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-
+    if (existingProfileIndex >= 0) {
+      const existingProfile = profiles[existingProfileIndex];
+      if (JSON.stringify(existingProfile) === JSON.stringify(profile)) {
+        alert("Profile with the same name and the same values already exists");
+      } else {
+        axios.put(`http://localhost:3001/data/${profile.title}`, profile)
+          .then(response => {
+            console.log(response.data);
+            const updatedProfiles = [...profiles];
+            updatedProfiles[existingProfileIndex] = response.data.updatedProfile;
+            setProfiles(updatedProfiles);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      }
+    } else {
+      axios.post('http://localhost:3001/data', profile)
+        .then(response => {
+          console.log(response.data);
+          setProfiles(oldProfiles => [...oldProfiles, profile]);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
   };
 
   const handleSelect = (event) => {
-    const selectedProfile = profiles.find(profile => profile.title === event.target.value);
+    const selectedProfileTitle = event.target.value;
+    const selectedProfile = profiles.find(profile => profile.title === selectedProfileTitle);
     setProfile(selectedProfile);
   };
 
-
- // Only render the component if profiles have been fetched
-if (profile === null) {
-  return <div>No profiles available.</div>;
-} else if (!profile) {
-  return <div>Loading...</div>;
-}
-
+  if (profile === null) {
+    return <div>No profiles available.</div>;
+  } else if (!profile) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="settings-container">
-      <h2 className="settings-title">Settings</h2>
-      
-      <div className="profile-selection">
-        <label htmlFor="profile-select">Select Profile:</label>
-        <select id="profile-select" onChange={handleSelect} value={profile.title}>
-          {profiles.map((profile, index) => (
-            <option key={index} value={profile.title}>{profile.title}</option>
-          ))}
-        </select>
-      </div>
+    <div className="profile">
+      <Sidebar className="sidebar" /> {/* This is the sidebar component */}
 
-      <div className="setting-item">
-        <label htmlFor="title-input">Name:</label>
-        <input
-          id="title-input"
-          type="text"
-          placeholder="Name of your profile"
-          name="title"
-          value={profile.title}
-          onChange={handleChange}
-        />
-      </div>
+      <div className="settings-container">
+        <div className="profileContainer">
+          <h2 className="settings-title">Settings</h2>
 
-      <div className="setting-item">
-        <label htmlFor="co2-min-input">CO2 Min Value:</label>
-        <input
-          id="co2-min-input"
-          type="number"
-          placeholder="CO2 Min Value"
-          name="co2Min"
-          value={profile.co2Min || ""}
-          onChange={handleChange}
-        />
-      </div>
+          <div className="profile-selection">
+            <label htmlFor="profile-select">Select Profile:</label>
+            <select id="profile-select" onChange={handleSelect} value={profile.title}>
+              {profiles.map((profile, index) => (
+                <option key={index} value={profile.title}>{profile.title}</option>
+              ))}
+            </select>
+          </div>
 
-      <div className="setting-item">
-        <label htmlFor="co2-max-input">CO2 Max Value:</label>
-        <input
-          id="co2-max-input"
-          type="number"
-          placeholder="CO2 Max Value"
-          name="co2Max"
-          value={profile.co2Max || ""}
-          onChange={handleChange}
-        />
-      </div>
+          <div className="setting-item">
+            <label htmlFor="title-input">Name:</label>
+            <input
+              id="title-input"
+              type="text"
+              placeholder="Name of your profile"
+              name="title"
+              value={profile.title}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div className="setting-item">
-        <label htmlFor="humidity-min-input">Humidity Min Value:</label>
-        <input
-          id="humidity-min-input"
-          type="number"
-          placeholder="Humidity Min Value"
-          name="humidityMin"
-          value={profile.humidityMin || ""}
-          onChange={handleChange}
-        />
-      </div>
+          <div className="setting-item">
+            <label htmlFor="co2-min-input">CO2 Min Value:</label>
+            <input
+              id="co2-min-input"
+              type="number"
+              placeholder="CO2 Min Value"
+              name="co2Min"
+              value={profile.co2Min || ""}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div className="setting-item">
-        <label htmlFor="humidity-max-input">Humidity Max Value:</label>
-        <input
-          id="humidity-max-input"
-          type="number"
-          placeholder="Humidity Max Value"
-          name="humidityMax"
-          value={profile.humidityMax || ""}
-          onChange={handleChange}
-        />
-      </div>
+          <div className="setting-item">
+            <label htmlFor="co2-max-input">CO2 Max Value:</label>
+            <input
+              id="co2-max-input"
+              type="number"
+              placeholder="CO2 Max Value"
+              name="co2Max"
+              value={profile.co2Max || ""}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div className="setting-item">
-        <label htmlFor="temperature-min-input">Temperature Min Value:</label>
-        <input
-          id="temperature-min-input"
-          type="number"
-          placeholder="Temperature Min Value"
-          name="temperatureMin"
-          value={profile.temperatureMin || ""}
-          onChange={handleChange}
-        />
-      </div>
+          <div className="setting-item">
+            <label htmlFor="humidity-min-input">Humidity Min Value:</label>
+            <input
+              id="humidity-min-input"
+              type="number"
+              placeholder="Humidity Min Value"
+              name="humidityMin"
+              value={profile.humidityMin || ""}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div className="setting-item">
-        <label htmlFor="temperature-max-input">Temperature Max Value:</label>
-        <input
-          id="temperature-max-input"
-          type="number"
-          placeholder="Temperature Max Value"
-          name="temperatureMax"
-          value={profile.temperatureMax || ""}
-          onChange={handleChange}
-        />
-      </div>
+          <div className="setting-item">
+            <label htmlFor="humidity-max-input">Humidity Max Value:</label>
+            <input
+              id="humidity-max-input"
+              type="number"
+              placeholder="Humidity Max Value"
+              name="humidityMax"
+              value={profile.humidityMax || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="temperature-min-input">Temperature Min Value:</label>
+            <input
+              id="temperature-min-input"
+              type="number"
+              placeholder="Temperature Min Value"
+              name="temperatureMin"
+              value={profile.temperatureMin || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="temperature-max-input">Temperature Max Value:</label>
+            <input
+              id="temperature-max-input"
+              type="number"
+              placeholder="Temperature Max Value"
+              name="temperatureMax"
+              value={profile.temperatureMax || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="setting-item">
+            <label htmlFor="servo-function">Servo Function:</label>
+            <input
+              id="servo-function"
+              type="number"
+              placeholder="Input a value between -100 and 100"
+              name="servoFunction"
+              value={profile.servoFunction || ""}
+              onChange={handleChange}
+              min="-100"
+              max="100"
+            />
+          </div>
 
 
-      <div className="button-container">
-        <button onClick={handleDiscard}>Discard</button>
-        <button onClick={handleSave}>Save</button>
+          <div className="button-container">
+            <button onClick={handleDiscard}>Discard</button>
+            <button onClick={handleSave}>Save</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -8,21 +8,61 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-//data used for the signin function in the login component
-/*
-app.get("/login-data", (req, res) => {
-  fs.readFile("./logindata.json", "utf8", (err, data) => {
+app.get("/data/:title", (req, res) => {
+  const title = req.params.title;
+
+  fs.readFile("./data2.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send(err);
     } else {
-      res.send(JSON.parse(data));
+      let existingData = JSON.parse(data);
+      const profile = existingData.find((profile) => profile.title === title);
+
+      if (profile) {
+        res.send(profile);
+      } else {
+        res.status(404).send({ status: "Profile not found" });
+      }
     }
   });
 });
-*/
 
-//data used for reading profile-presets
+app.put("/data/:title", (req, res) => {
+  const title = req.params.title;
+  const newData = req.body;
+
+  fs.readFile("./data2.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      let existingData = JSON.parse(data);
+      const index = existingData.findIndex(
+        (profile) => profile.title === title
+      );
+
+      if (index > -1) {
+        existingData[index] = newData;
+        fs.writeFile(
+          "./data2.json",
+          JSON.stringify(existingData, null, 2),
+          (err) => {
+            if (err) {
+              console.error(err);
+              res.status(500).send(err);
+            } else {
+              res.send({ status: "success", updatedProfile: newData });
+            }
+          }
+        );
+      } else {
+        res.status(404).send({ status: "Profile not found" });
+      }
+    }
+  });
+});
+
 app.get("/data", (req, res) => {
   fs.readFile("./data2.json", "utf8", (err, data) => {
     if (err) {
@@ -34,59 +74,6 @@ app.get("/data", (req, res) => {
   });
 });
 
-//data used for writiing to a temporry file for fetching using modern browser fetch
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  fs.readFile("./logindata.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err);
-    } else {
-      const users = JSON.parse(data);
-      const user = users.find(
-        (user) => user.email === email && user.password === password
-      );
-      if (user) {
-        res.send({ status: "success", message: "Login successful" });
-      } else {
-        res
-          .status(401)
-          .send({ status: "failure", message: "Invalid email or password" });
-      }
-    }
-  });
-});
-
-//for signing up
-app.post("/signup", (req, res) => {
-  const { email, password } = req.body;
-  fs.readFile("./logindata.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err);
-    } else {
-      const users = JSON.parse(data);
-      const userExists = users.find((user) => user.email === email);
-      if (userExists) {
-        res.status(409).send({ status: "failure", message: "Email already exists" });
-      } else {
-        users.push({ email, password });
-        fs.writeFile("./logindata.json", JSON.stringify(users, null, 2), 'utf8', (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send(err);
-          } else {
-            res.send({ status: "success", message: "Signup successful" });
-          }
-        });
-      }
-    }
-  });
-});
-
-
-
-//data used for writing profile-presets
 app.post("/data", (req, res) => {
   const newData = req.body;
   fs.readFile("./data2.json", "utf8", (err, data) => {
