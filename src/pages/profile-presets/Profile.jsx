700ff48a6ec0f394a40f7ce9dc9af0f009b33f80
Gabriel Vlad <gabriel.vlad@outlook.dk>
2023-05-29 00:44:10 +0200

    small fix still needed

--- src/pages/profile-presets/Profile.jsx
+++ src/pages/profile-presets/Profile.jsx
@@ -11,24 +11,24 @@ const Profile = () => {
   
   const handleChange = (event) => {
     const { name, value } = event.target;
-    console.log(`handleChange event - name: ${name}, value: ${value}`); // Added log
+    console.log(`handleChange event - name: ${name}, value: ${value}`);  
     setProfile(prevProfile => {
       const updatedProfile = { ...prevProfile, [name]: value };
-      console.log('Updated profile in handleChange:', updatedProfile); // Added log
+      console.log('Updated profile in handleChange:', updatedProfile);  
       return updatedProfile;
     });
   };
 
   //connects to backend and gets all profiles following the structure under it, since this is what the backend returns
   useEffect(() => {
-    console.log('useEffect triggered'); // Added log
+    console.log('useEffect triggered');  
     axios.get('https://backend-esqp5xwphq-od.a.run.app/api/presets/1', {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`
         }
       })
       .then(response => {
-        console.log('Axios get response:', response.data); // Added log
+        console.log('Axios get response:', response.data);  
         setProfiles(response.data);
         setProfile(response.data[0]);
       })
@@ -37,109 +37,150 @@ const Profile = () => {
       });
   }, []);
 
-  const createNewProfile = () => {
-    console.log('Creating new profile'); // Added log
+  const createNewProfile = (name, minHumidity, maxHumidity, minCo2, maxCo2, minTemperature, maxTemperature, servo) => {
+    console.log('Creating new profile');
     return {
       id: 0,
-      name: '',
-      minHumidity: 0,
-      maxHumidity: 0,
-      minCo2: 0,
-      maxCo2: 0,
-      minTemperature: 0,
-      maxTemperature: 0,
-      servo: -100,
+      name: name,
+      minHumidity: minHumidity,
+      maxHumidity: maxHumidity,
+      minCo2: minCo2,
+      maxCo2: maxCo2,
+      minTemperature: minTemperature,
+      maxTemperature: maxTemperature,
+      servo: servo,
       deviceId: 1,
       device: null
     };
   };
-
-  //Handles the add of a new profile to the backend
+  
+  
   const handleAdd = () => {
-    if (!profile) return;
-    const newProfile = { ...profile };
-    axios.post('https://backend-esqp5xwphq-od.a.run.app/api/presets/create', newProfile, {
+    console.log('Creating new profile');
+    const newProfile = createNewProfile(
+      profile.name,
+      profile.minHumidity,
+      profile.maxHumidity,
+      profile.minCo2,
+      profile.maxCo2,
+      profile.minTemperature,
+      profile.maxTemperature,
+      profile.servo
+    );
+  
+    axios.post("https://backend-esqp5xwphq-od.a.run.app/api/presets/create", newProfile, {
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
     })
-      .then(response => {
+      .then((response) => {
         const addedProfile = response.data;
-        setProfiles(oldProfiles => [...oldProfiles, addedProfile]);
-        setProfile(addedProfile);
+        setProfiles((oldProfiles) => [...oldProfiles, addedProfile]);
+        setProfile(addedProfile); // Set the newly created profile as the current profile
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
   };
   
+  
+  
+
   //Handles the save to the backend
   const handleSave = () => {
-    console.log('Saving profile:', profile); // Added log
-    // if profile is null, create a new profile
+    console.log('Saving profile:', profile);
+  
     if (!profile) {
-      setProfile(createNewProfile());
+      console.log("No profile selected to save.");
       return;
     }
-
-    // Here we create a copy of the profile object to ensure that it has the most recent changes
+  
     const profileToUpdate = {...profile};
-
+  
     const existingProfileIndex = profiles.findIndex((p) => p.id === profile.id);
   
-    if (existingProfileIndex >= 0) { // The profile already exists, so we should update it
+    if (existingProfileIndex >= 0) { 
       axios.put(`https://backend-esqp5xwphq-od.a.run.app/api/presets/${profile.id}`, profileToUpdate, {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`
         }
       })
         .then(response => {
-          console.log('Axios put response:', response.data); // Added log
-          const updatedProfile = response.data;
-          const updatedProfiles = profiles.map(p => p.id === updatedProfile.id ? updatedProfile : p);
-          setProfiles(updatedProfiles);
-          setProfile(updatedProfile);
+          console.log('Axios put response:', response.data);
+          
+          // Refetch the profiles after a successful save
+          axios.get('https://backend-esqp5xwphq-od.a.run.app/api/presets/1', {
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+          })
+            .then(response => {
+              console.log('Axios get response:', response.data);  
+              setProfiles(response.data);
+              const updatedProfile = response.data.find(p => p.id === profile.id); // find the updated profile in the list
+              setProfile(updatedProfile);
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
         })
         .catch(error => {
           console.error('Error:', error);
         });
-    } else { // The profile does not exist, so we should create it
-      axios.post('https://backend-esqp5xwphq-od.a.run.app/api/presets/create', profileToUpdate, {
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
-      })
-        .then(response => {
-          console.log('Axios post response:', response.data); // Added log
-          const newProfile = response.data;
-          setProfiles(oldProfiles => [...oldProfiles, newProfile]);
-          setProfile(newProfile);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+    } else {
+      console.log("No matching profile found to update.");
     }
   };
+  
+  
 
   //Handles the selecting from the backend using a dropdown menu
   const handleSelect = (event) => {
     const selectedProfileId = parseInt(event.target.value);
-    console.log(`handleSelect event - selected profile id: ${selectedProfileId}`); // Added log
-    const selectedProfile = profiles.find(profile => profile.id === selectedProfileId);
+    console.log(`handleSelect event - selected profile id: ${selectedProfileId}`);
+    const selectedProfile = profiles.find((profile) => profile.id === selectedProfileId);
     setProfile(selectedProfile);
   };
 
   //Handles the activation of the profile, should send the id of the profile to the backend
   const handleActivate = (profileToActivate) => {
-    console.log('Activating profile:', profileToActivate); // Added log
+    console.log('Activating profile:', profileToActivate);
     setActiveProfile(profileToActivate);
     // TODO: send to backend when we have the endpoint
   };
 
   //Handles the deletion of the profile, should request the deletion from the backend on the same endpoint as the GET
   const handleDiscard = () => {
-    console.log('Discard button clicked'); // Added log
-    // TODO: send to backend when we have the endpoint
+    console.log('Discarding profile:', profile);
+  
+    if (!profile) {
+      console.log("No profile selected to discard.");
+      return;
+    }
+  
+    axios.delete(`https://backend-esqp5xwphq-od.a.run.app/api/presets/${profile.id}`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      .then(response => {
+        console.log('Axios delete response:', response.data);  
+        const remainingProfiles = profiles.filter(p => p.id !== profile.id);
+        setProfiles(remainingProfiles);
+        
+        if (remainingProfiles.length > 0) {
+          setProfile(remainingProfiles[0]);  // sets the first profile from the remaining profiles
+        } else {
+          setProfile(null);
+        }
+  
+        if (activeProfile?.id === profile.id) {
+          setActiveProfile(null);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   };
   
 
@@ -165,11 +206,11 @@ const Profile = () => {
               onChange={handleSelect}
               value={profile.id}
             >
-              {profiles.map((profile) => (
-                <option key={profile.id} value={profile.id}>
-                  {profile.name}
-                </option>
-              ))}
+            {profiles.map((profile) => (
+            <option key={profile.id} value={profile.id}>
+              {profile.name}
+            </option>
+            ))}
             </select>
             <button
               className={`activateButton ${profile.id === activeProfile?.id ? "active" : ""}`}
