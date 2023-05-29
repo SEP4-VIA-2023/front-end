c0d3b703bc2674d36ceb6a037e00d39bd3fe21fa
Gabriel Vlad <gabriel.vlad@outlook.dk>
2023-05-29 15:50:35 +0200

    cleanup and fixed failing tests
    cleanup

--- src/components/login/SignIn.test.js
+++ src/components/login/SignIn.test.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import SignIn from './SignIn';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import SignIn from "./SignIn";
 
-// mock FormData
-global.FormData = jest.fn(() => ({
-  get: jest.fn().mockReturnValue('test-value'),
-}));
+describe("<SignIn />", () => {
+  it("renders the SignIn component", () => {
+    render(
+      <MemoryRouter>
+        <SignIn />
+      </MemoryRouter>
+    );
 
-describe('<SignIn />', () => {
-  it('renders the SignIn component', () => {
-    render(<SignIn />);
-    expect(screen.getByText('Sign in')).toBeInTheDocument();
-    expect(screen.getByText('Email Address')).toBeInTheDocument();
-    expect(screen.getByText('Password')).toBeInTheDocument();
-    expect(screen.getByText('Remember me')).toBeInTheDocument();
-    expect(screen.getByText('Sign In')).toBeInTheDocument();
-    expect(screen.getByText("Don't have an account? Sign Up")).toBeInTheDocument();
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByLabelText("Email Address *")).toBeInTheDocument();
+    expect(screen.getByLabelText("Password *")).toBeInTheDocument();
+    expect(screen.getByText("Remember me")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Sign In/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Don't have an account? Sign Up")
+    ).toBeInTheDocument();
   });
-
 });
