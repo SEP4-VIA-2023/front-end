65c423b2cd5637a7e7ab5219056f350f2003a8ee
Bhupas Gautam <bhupas@outlook.com>
2023-05-30 13:00:12 +0200

    fixed time bug

--- src/components/charts/UseFormattedData.js
+++ src/components/charts/UseFormattedData.js
@@ -4,13 +4,12 @@ function UseFormattedData(data, metricUnits) {
   const [formattedData, setFormattedData] = useState([]);
 
   useEffect(() => {
-    const formatDate = (time) => new Date(time).toLocaleString();
 
     const convertData = (data, metric) => data.map(({ time, temperature, humidity, co2 }) => {
       const formattedTemperature = metric ? temperature : (temperature * 1.8 + 32);
 
       return {
-        time: formatDate(time),
+        time: time, // no formatting of time here
         humidity: humidity,
         temperature: formattedTemperature.toFixed(1),
         co2: co2,
